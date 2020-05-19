e8bc0bec50c18f8a04a6b6d12ffd2d0f4f17a213
EduardaB <eduardacbianquine@gmail.com>
2020-05-19 08:41:30 -0300

    Score de volume de compras

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int valC, at;
+    int valC, at, pts;
     double tm;
     string tp;
 
@@ -24,5 +24,23 @@ int main()
     cin >> at;
     cout << "A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ";
     cin >> tp;
+
+    //Pt.4: Score volume
+    if(tm==0)
+    {
+      pts = 0;
+    }
+    if(valC<=3000 && tm<=2)
+    {
+      pts = 20;
+    }
+    else if(valC<=3000 && tm>2)
+    {
+      pts = 40;
+    else if(valC>3000)
+    {
+    pts = 60;
+    }
+    cout << endl << "Score de volume de compras = " << pts << " pontos" << endl;
     return 0;
 }
