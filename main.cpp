6634acc02b3a39cbdbd82a634cecdeab8130dde0
EduardaB <eduardacbianquine@gmail.com>
2020-05-19 08:28:24 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@ using namespace std;
 
 int main()
 {
+    int valC;
+    double tm;
+
     // Pt. 1: cabecalho
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
     cout << "INFORME OS DADOS DO ÚLTIMO ANO" << endl;
+
+    // Pt. 2: leitura do volume
+    cout << endl << "Quantas compras o cliente fez no último ano? ";
+    cin >> valC;
+    cout << "Qual o ticket médio? ";
+    cin >> tm;
     return 0;
 }
